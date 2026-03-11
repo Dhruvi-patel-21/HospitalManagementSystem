1708f6e9e7d4cf107ddec5d29183197345e143e2
Dhruvi-patel-21 <dhruvipanchotiya219@gmail.com>
2026-03-11 15:12:23 +0530

    Appointment.java

--- src/java/model/Appointment.java
+++ src/java/model/Appointment.java
@@ -7,11 +7,10 @@ public class Appointment {
     private int doctorId;
     private String date;
 
-    // 🔹 NEW fields (ONLY for VIEW purpose)
+   
     private String patientName;
     private String doctorName;
 
-    // ===== getters & setters =====
 
     public int getId() {
         return id;
@@ -41,7 +40,7 @@ public class Appointment {
         this.date = date;
     }
 
-    // ===== NEW getters & setters =====
+   
 
     public String getPatientName() {
         return patientName;
@@ -59,38 +58,3 @@ public class Appointment {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*package model;
-
-public class Appointment{
-    private int id, patientId, doctorId;
-    private String date;
-
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public int getPatientId() { return patientId; }
-    public void setPatientId(int patientId) { this.patientId = patientId; }
-
-    public int getDoctorId() { return doctorId; }
-    public void setDoctorId(int doctorId) { this.doctorId = doctorId; }
-
-    public String getDate() { return date; }
-    public void setDate(String date) { this.date = date; }
-}*/
